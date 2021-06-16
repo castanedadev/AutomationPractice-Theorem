1af1005420ed6764f872ffea6162114e5d9beb4b
Rick C <rickcastaneda@outlook.com>
2021-06-16 03:09:41 -0600

    T003: Add enums for categories available in the site :construction:

---
+++ src/constants/existingCategories.js
@@ -0,0 +1,7 @@
+const Category = {
+  WOMEN: 'Women',
+  DRESSES: 'Dresses',
+  T_SHIRTS: 'T-shirts',
+};
+
+export default Category;
---
+++ src/constants/existingSubCategories.js
@@ -0,0 +1,13 @@
+const SubCategory = {
+  WOMEN: {
+    TOPS: 'Tops',
+    DRESSES: 'Dresses',
+  },
+  DRESSES: {
+    CASUAL_DRESSES: 'Casual Dresses',
+    EVENING_DRESSES: 'Evening Dresses',
+    SUMMER_DRESSES: 'Summer Dresses',
+  },
+};
+
+export default SubCategory;
